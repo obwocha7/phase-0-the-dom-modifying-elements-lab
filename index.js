19da61bf02350141a6866f178cccaa58e6862a2a
Derrick <omoriderrick@gmail.com>
2024-04-10 06:55:14 +0300

    New Feat: Changing Dom Elements

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const main = document.querySelector('#main');
+main.remove();
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = 'victory';
+newHeader.innerHTML = `<p>Derrick is the champion</p>`;
